bee80879acc301023a9a8da451947b71b01daa49
kaede <kaede@KaedeMac.local>
2017-03-27 00:11:44 +0800

    顧名思義

---
+++ json2mysql.py
@@ -0,0 +1,41 @@
+# coding=UTF-8
+# 記得 conda install pymysql 或 pip install pymysql
+import pymysql.cursors
+import json
+
+# 設定參數
+filename = '會員資料.json'
+tablename = 'test'
+obj = json.load(file(filename))
+connection = pymysql.connect(host='localhost',
+                             user='workout',
+                             password='workout',
+                             db='workout',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+# 建立表格
+x = obj['data'][0]
+keys = []
+for k in x.keys():
+    keys += ["`%s` text" % k]
+sql = "create table `%s` (%s)"
+with connection.cursor() as cursor:
+        cursor.execute("drop table if exists `%s`" % tablename)
+        cursor.execute(sql % (tablename, ",".join(keys)))
+
+# 匯入資料
+keys = []
+vars = []
+for k in x.keys():
+    keys += [k]
+    vars += ['%s']
+sql = "insert into `%s` (`%s`) values (%s)" % (tablename, "`,`".join(keys), ",".join(vars))
+for x in obj['data']:
+    vals = []
+    for k in x.keys():
+        vals += [x[k]]
+    with connection.cursor() as cursor:
+        cursor.execute(sql, vals)
+connection.commit()
+
