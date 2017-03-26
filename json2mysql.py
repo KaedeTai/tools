0d9ca8f36b4132895e94848246435f77a716525a
kaede <kaede@KaedeMac.local>
2017-03-27 00:39:35 +0800

    調整格式

--- json2mysql.py
+++ json2mysql.py
@@ -5,24 +5,19 @@ import json
 
 # 設定參數
 filename = '會員資料.json'
-tablename = 'test'
-obj = json.load(file(filename))
-connection = pymysql.connect(host='localhost',
-                             user='workout',
-                             password='workout',
-                             db='workout',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+tablename = 'member'
+connection = pymysql.connect(host='localhost', user='workout', password='workout', db='workout', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
 
 # 建立表格
+obj = json.load(file(filename))
 x = obj['data'][0]
 keys = []
 for k in x.keys():
     keys += ["`%s` text" % k]
 sql = "create table `%s` (%s)"
 with connection.cursor() as cursor:
-        cursor.execute("drop table if exists `%s`" % tablename)
-        cursor.execute(sql % (tablename, ",".join(keys)))
+    cursor.execute("drop table if exists `%s`" % tablename)
+    cursor.execute(sql % (tablename, ",".join(keys)))
 
 # 匯入資料
 keys = []
